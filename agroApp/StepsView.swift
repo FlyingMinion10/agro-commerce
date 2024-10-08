f7dd38e1396573fb7e01f3b47cad03a8131b991d
FlyingMinion10 <a01641980@tec.mx>
2024-10-08 10:36:44 -0600

    links deshabilitados por pasos

--- agroApp/StepsView.swift
+++ agroApp/StepsView.swift
@@ -9,12 +9,13 @@ struct StepsView: View {
     // Variables heredadas desde ChatsView para la negociación
     var interaction_id: Int
     var publication_id: Int
+    var item_preview: String
     var buyer: String
     var seller: String
+    var currentStep: Int
     
     // Importar elemento para volver a la vista anterior
     @Environment(\.dismiss) var dismiss
-
     var body: some View {
         NavigationView {
             VStack {
@@ -40,7 +41,6 @@ struct StepsView: View {
                 .padding()
                 .frame(width: screenWidth, height: 60)
                 .background(Color.white)
-                
                 ScrollView {
                     if myAccountType == "Bodeguero" {
                         // MARK: - STEPS DEL BODEGUERO
@@ -49,86 +49,87 @@ struct StepsView: View {
                                 Image(systemName: "lock")
                                 Text("Ubicación Rancho")
                             }
-                            .lockedStepStyle()
+                            .lockedStepStyle(currentStep: currentStep, step: 1)
                         
-                            NavigationLink(destination: BodegaStep(interaction_id: interaction_id)) {
+                            NavigationLink(destination: BodegaStep(interaction_id: interaction_id, step: currentStep)) {
                                 HStack {
                                     Text("Ubicación bodega")
                                 }
                             }
-                            .activeStepStyle(color: .blue)
+                            .activeStepStyle(color: .blue, currentStep: currentStep, step: 2)
                             
-                            HStack {
-                                Image(systemName: "lock")
-                                Text("Contrato productor")
+                            NavigationLink(destination: TransporteStep(interaction_id: interaction_id, publication_id: publication_id, item_preview: item_preview)) {
+                                HStack {
+                                    Text("Negociacion transporte")
+                                }
                             }
-                            .lockedStepStyle()
+                            .activeStepStyle(color: .green, currentStep: currentStep, step: 3)
                             
                             NavigationLink(destination: Paso4View()) {
                                 HStack {
                                     Text("Contrato bodeguero")
                                 }
                             }
-                            .activeStepStyle(color: .yellow)
+                            .activeStepStyle(color: .yellow, currentStep: currentStep, step: 4)
                             
                             NavigationLink(destination: Paso5View()) {
                                 HStack {
                                     Text("Paso 5")
                                 }
                             }
-                            .activeStepStyle(color: .purple)
+                            .activeStepStyle(color: .purple, currentStep: currentStep, step: 5)
                             
                             NavigationLink(destination: Paso6View()) {
                                 HStack {
                                     Text("Paso 6")
                                 }
                             }
-                            .activeStepStyle(color: .orange)
+                            .activeStepStyle(color: .orange, currentStep: currentStep, step: 6)
                         }
                         .padding(.top, 20)
                         .frame(width: screenWidth, height: .infinity)
                     } else {
-                        // MARK: - // MARK: - STEPS DEL AGRICULTOR
+                        // MARK: - STEPS DEL AGRICULTOR
                         VStack(spacing: 20) {
                             NavigationLink(destination: RanchoStep(interaction_id: interaction_id)) {
                                 HStack {
                                     Text("Ubicación Rancho")
                                 }
                             }
-                            .activeStepStyle(color: .red)
+                            .activeStepStyle(color: .red, currentStep: currentStep, step: 1)
                             
                             HStack {
                                     Image(systemName: "lock")
                                     Text("Ubicación bodega")
                             }
-                            .lockedStepStyle()
+                            .lockedStepStyle(currentStep: currentStep, step: 2)
                             
-                            NavigationLink(destination: Paso3View()) {
+                            NavigationLink(destination: TransporteStep(interaction_id: interaction_id, publication_id: publication_id, item_preview: item_preview)) {
                                 HStack {
-                                    Text("Contrato productor")
+                                    Text("Negociacion transporte")
                                 }
                             }
-                            .activeStepStyle(color: .green)
+                            .activeStepStyle(color: .green, currentStep: currentStep, step: 3)
                             
                             HStack {
                                     Image(systemName: "lock")
                                     Text("Contrato bodeguero")
                             }
-                            .lockedStepStyle()
+                            .lockedStepStyle(currentStep: currentStep, step: 4)
                             
                             NavigationLink(destination: Paso5View()) {
                                 HStack {
                                     Text("Paso 5")
                                 }
                             }
-                            .activeStepStyle(color: .purple)
+                            .activeStepStyle(color: .purple, currentStep: currentStep, step: 5)
                             
                             NavigationLink(destination: Paso6View()) {
                                 HStack {
                                     Text("Paso 6")
                                 }
                             }
-                            .activeStepStyle(color: .orange)
+                            .activeStepStyle(color: .orange, currentStep: currentStep, step: 6)
                         }
                         .padding(.top, 20)
                         .frame(width: screenWidth, height: .infinity)
@@ -142,31 +143,31 @@ struct StepsView: View {
 }
 
 extension View {
-    func activeStepStyle(color: Color) -> some View {
+    func activeStepStyle(color: Color, currentStep: Int, step: Int) -> some View {
         self
             .frame(width: 330, height: 65)
             .background(Color.white)
-            .foregroundStyle(Color.black)
+            .foregroundStyle(Color.black.opacity(currentStep < step ? 0.5 : 1))
             .font(.system(size: 25))
             .clipShape(RoundedRectangle(cornerRadius: 16))
+            .disabled(currentStep < step)
             .overlay(
                 RoundedRectangle(cornerRadius: 16)
-                    .stroke(color, lineWidth: 2)
+                    .stroke(color.opacity(currentStep < step ? 0.5 : 1), lineWidth: 2)
             )
     }
 }
 
 extension View {
-    func lockedStepStyle() -> some View {
+    func lockedStepStyle(currentStep: Int, step: Int) -> some View {
         self
             .frame(width: 330, height: 65)
-            .background(Color.black.opacity(0.2))
-            .foregroundStyle(Color.black)
+            .foregroundStyle(Color.black.opacity(currentStep < step ? 0.5 : 1))
             .font(.system(size: 25))
             .clipShape(RoundedRectangle(cornerRadius: 16))
             .overlay(
                 RoundedRectangle(cornerRadius: 16)
-                    .stroke(Color.gray, lineWidth: 2)
+                    .stroke(Color.black.opacity(currentStep < step ? 0.5 : 1), lineWidth: 2)
             )
     }
 }
@@ -180,12 +181,6 @@ struct Paso1View: View {
 struct Paso2View: View {
     var body: some View {
         Text("Paso dos insption")
-    }
-}
-
-struct Paso3View: View {
-    var body: some View {
-        Text("Paso tres insption")
     }
 }
 
@@ -209,6 +204,6 @@ struct Paso6View: View {
 
 struct StepsView_Previews: PreviewProvider {
     static var previews: some View {
-        StepsView(interaction_id: 1, publication_id: 30, buyer: "p", seller: "j")
+        StepsView(interaction_id: 3, publication_id: 30, item_preview: "Aguacate", buyer: "p", seller: "j", currentStep: 4)
     }
 }
