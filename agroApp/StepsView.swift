9ff615bfbdbdc6e2a15759c81d96761dfe66f50c
FlyingMinion10 <zepedajuanfe9@gmail.com>
2024-10-28 10:35:19 -0600

    Antes del cambio de colores

--- agroApp/StepsView.swift
+++ agroApp/StepsView.swift
@@ -1,5 +1,62 @@
 import SwiftUI
 
+struct InfoTutoRec: View {
+    let number: Int
+    let text: String
+    let description: String
+    
+    var body: some View {
+        HStack(alignment: .top) {
+            Text("\(number)")
+                .font(.system(size: 24, weight: .bold))
+                .frame(width: 30, height: 30)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(15)
+                .padding(.top, 5)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(text)
+                    .font(.headline)
+                Text(description)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+        }
+    }
+}
+
+struct dismiss_header: View {
+    var title: String
+    let screenWidth = UIScreen.main.bounds.width
+    @Environment(\.dismiss) var dismiss
+    var body: some View {
+        HStack{
+            Button(action: {
+                dismiss()
+            }) {
+                Image(systemName: "chevron.backward")
+                    .font(.system(size: 24))
+            }
+            Spacer()
+            Text(title)
+                .font(.system(size: 30))
+                .foregroundStyle(Color.black.opacity(0.9))
+                .padding(.vertical, 20)
+            Spacer()
+            Button(action: {
+            }) {
+                Image(systemName: "ellipsis")
+                    .font(.system(size: 24))
+            }
+        }
+        .padding()
+        .frame(width: screenWidth, height: 60)
+        .background(Color.white)
+
+    }
+}
+
 struct StepsView: View {
     let screenWidth = UIScreen.main.bounds.width
     
@@ -100,7 +157,7 @@ struct StepsView: View {
                             }
                             .activeStepStyle(color: .blue, currentStep: currentStep, step: 8)
 
-                            NavigationLink(destination: Paso9View()) {
+                            NavigationLink(destination: ContratoModStep(interaction_id: interaction_id, step: currentStep)) {
                                 HStack {
                                     Text("Contrato Modificado")
                                 }
@@ -175,6 +232,75 @@ struct StepsView: View {
                                 }
                             }
                             .activeStepStyle(color: .green, currentStep: currentStep, step: 4)
+                            NavigationLink(destination: TransportistaPrim(tranPrimStep: 2)) {
+                                HStack {
+                                    Text("Transportista 1 (1-3)")
+                                }
+                            }
+                            .activeStepStyle(color: .cyan, currentStep: currentStep, step: 5)
+
+                            NavigationLink(destination: PagoSTPStep()) {
+                                HStack {
+                                    Text("Pago STP")
+                                }
+                            }
+                            .activeStepStyle(color: .pink, currentStep: currentStep, step: 6)
+
+                            NavigationLink(destination: TransportistaSec(tranSecStep: 4)) {
+                                HStack {
+                                    Text("Transportista 2 (4-7)")
+                                }
+                            }
+                            .activeStepStyle(color: .purple, currentStep: currentStep, step: 7)
+
+                            NavigationLink(destination: InspeccionCorreccion()) {
+                                HStack {
+                                    Text("Inspeción Corrección")
+                                }
+                            }
+                            .activeStepStyle(color: .blue, currentStep: currentStep, step: 8)
+
+                            NavigationLink(destination: ContratoModStep(interaction_id: interaction_id, step: currentStep)) {
+                                HStack {
+                                    Text("Contrato Modificado")
+                                }
+                            }
+                            .activeStepStyle(color: .gray, currentStep: currentStep, step: 9)
+
+                            NavigationLink(destination: TransportistaTer(tranTerStep: 5)) {
+                                HStack {
+                                    Text("Transportista 3 (8-13)")
+                                }
+                            }
+                            .activeStepStyle(color: .yellow, currentStep: currentStep, step: 10)
+
+                            NavigationLink(destination: LiberarPago()) {
+                                HStack {
+                                    Text("Liberar Pago")
+                                }
+                            }
+                            .activeStepStyle(color: .orange, currentStep: currentStep, step: 11)
+
+                            NavigationLink(destination: RatingPage()) {
+                                HStack {
+                                    Text("Rating")
+                                }
+                            }
+                            .activeStepStyle(color: .pink, currentStep: currentStep, step: 12)
+
+                            NavigationLink(destination: FacturaCompraVenta()) {
+                                HStack {
+                                    Text("Factura compra-venta")
+                                }
+                            }
+                            .activeStepStyle(color: .cyan, currentStep: currentStep, step: 13)
+
+                            NavigationLink(destination: FinalizarTransaccion()) {
+                                HStack {
+                                    Text("Finalizar Transacción")
+                                }
+                            }
+                            .activeStepStyle(color: .gray, currentStep: currentStep, step: 14)
                         }
                         .padding(.top, 20)
                         .frame(width: screenWidth, height: .infinity)
@@ -214,102 +340,6 @@ extension View {
                 RoundedRectangle(cornerRadius: 16)
                     .stroke(Color.black.opacity(currentStep < step ? 0.5 : 1), lineWidth: 2)
             )
-    }
-}
-
-struct Paso1View: View {
-    var body: some View {
-        Text("Paso uno insption")
-    }
-}
-
-struct Paso2View: View {
-    var body: some View {
-        Text("Paso dos insption")
-    }
-}
-
-struct Paso4View: View {
-    var body: some View {
-        Text("Paso cuatro insption")
-    }
-}
-
-struct Paso5View: View {
-    var body: some View {
-        Text("Paso cinco insption")
-    }
-}
-
-struct Paso6View: View {
-    var body: some View {
-        Text("Paso seis insption")
-    }
-}
-
-struct Paso7View: View {
-    var body: some View {
-        Text("Paso siete insption")
-    }
-}
-
-struct Paso8View: View {
-    var body: some View {
-        Text("Paso ocho insption")
-    }
-}
-
-struct Paso9View: View {
-    var body: some View {
-        Text("Paso nueve insption")
-    }
-}
-
-struct Paso10View: View {
-    var body: some View {
-        Text("Paso diez insption")
-    }
-}
-
-struct Paso11View: View {
-    var body: some View {
-        Text("Paso once insption")
-    }
-}
-
-struct Paso12View: View {
-    var body: some View {
-        Text("Paso doce insption")
-    }
-}
-
-struct Paso13View: View {
-    var body: some View {
-        Text("Transportista 3")
-    }
-}
-
-struct Paso14View: View {
-    var body: some View {
-        Text("Liberar Pago")
-    }
-}
-
-struct Paso15View: View {
-    var body: some View {
-        Text("Rating")
-    }
-}
-
-struct Paso16View: View {
-    var body: some View {
-        Text("Factura")
-    }
-}
-
-struct Paso17View: View {
-    var body: some View {
-        Text("Finalizar Transacción")
     }
 }
 
